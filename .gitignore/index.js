0edf9099dc46aae5351f76f2102f3df952f0f7d6
kruppatv <38617438+kruppatv@users.noreply.github.com>
2018-04-22 14:34:52 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js")
+
+var client = new Discord.Client();
+
+client.on("ready", fonction()
+)
+
+bot.login("NDM3NTYzMzk4OTEwMTE1ODQw.Db4Cfw.bwGqpz-xGhEsB5eID0aAKGkPb5M")
